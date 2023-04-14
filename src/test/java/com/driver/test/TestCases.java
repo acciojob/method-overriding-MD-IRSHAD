149fbc7d274c5a20bf686aef22aea044afb23645
MD-IRSHAD <114646710+MD-IRSHAD@users.noreply.github.com>
2023-04-15 00:00:42 +0530

    initial commit

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -2,7 +2,17 @@ package com.driver.test;
 
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
-
+class A{
+  public String a(){
+  return "Invoking method from class A";
+  }
+}
+class B extends A{
+  public String a(){
+  return "Method is overridden in Extendend class B";
+  }
+}
 public class TestCases {
-    
+    B obj=new B();
+    System.out.println(obj.a());
 }
